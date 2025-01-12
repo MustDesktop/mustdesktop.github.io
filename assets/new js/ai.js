a4beac21f53a2f720b9e460767a506e9ae2f991f
MustDesktop <mustdesktop@protonmail.com>
2025-01-13 01:13:07 +0200

    Add files via upload

---
+++ assets/new js/ai.js
@@ -0,0 +1,29 @@
+        function blackboxai() {
+            window.location.href = 
+                "https://www.blackbox.ai/";
+        }
+        function Deepseekai() {
+            window.location.href = 
+                "https://chat.deepseek.com/";
+        }
+        function ChatGpt() {
+            window.location.href = 
+                "https://chatgpt.com/";
+        }
+        function about() {
+            window.location.href = 
+                "about.html";
+        }
+
+        function gpac() {
+            window.location.href = 
+                "calc.html";
+        }
+        function exit() {
+            window.location.href = 
+                "index.html";
+        }
+        function dash() {
+            window.location.href = 
+                "ai.html";
+        }
